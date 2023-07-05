0b935e9116cc0caedc72f489c1a0a1a00dd36834
oguzhansecgel <oguzhansecgel19@icloud.com>
2023-07-05 23:18:37 +0300

    last commit

---
+++ EKutuphaneDataAccess/Concrete/Context.cs
@@ -0,0 +1,27 @@
+﻿using EKutuphaneCore.Abstract;
+using EKutuphaneCore.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EKutuphaneDataAccess.Concrete
+{
+	public class Context : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+		}
+
+        public DbSet<BaseAuthorInfos> BaseAuthorInfos { get; set; }
+        public DbSet<BaseEntity> BaseEntitys{ get; set; }
+		public DbSet<BaseInfos> BaseInfos { get; set; }
+		public DbSet<AuthorInfo> AuthorInfos { get; set; }
+		public DbSet<Book>Books { get; set; }
+		public DbSet<Manager> Managers { get; set; }
+		public DbSet<Member> Members { get; set; }
+    }
+}
